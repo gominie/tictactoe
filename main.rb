022d5dcaa07ca1930b6d3875514e30e9d8edfc72
gominie <109306648+gominie@users.noreply.com>
2024-02-16 22:09:40 +0000

    can now update board. player wont swap after 1st instance

--- main.rb
+++ main.rb
@@ -10,6 +10,7 @@
 class TicTacToe
   def initialize
     @@boardHash = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9 }
+    @@current_player = "X"
    end
 
   def self.drawBoard
@@ -24,17 +25,31 @@ class TicTacToe
     end
   end
 
+  def self.switch_player
+    @@current_player = (@@current_player = "X") ? "O" : "X"
+end
+
+  def self.update_board(choice)
+    @@boardHash[choice] = @@current_player
+  end
+
   def start_game
     puts 'Welcome to terminal tic tac toe!'
     # insert draw board module in a bit
-    
-    TicTacToe.drawBoard
     loop do
-      puts 'Player 1 select your choice'
-      guess = gets.chomp.to_i
+
+    TicTacToe.drawBoard
+      puts "Player #{@@current_player}, select your choice (1-9)"
+      choice = gets.chomp.to_i
+      if (1..9).include?(choice) && @@boardHash[choice]!= "X" && @@boardHash[choice] != "0"
+         TicTacToe.update_board(choice)
+         TicTacToe.switch_player
+       end
     end
   end
 end
+
+#rescue/exception code if user doesnt select 1..9
 
 game = TicTacToe.new
 game.start_game

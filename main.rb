23d36e60abfa334d37ed503b4d6bb061ff40fd04
gominie <109306648+gominie@users.noreply.com>
2024-02-19 18:23:22 +0000

    draw now works

--- main.rb
+++ main.rb
@@ -1,9 +1,12 @@
+require 'pry-byebug'
 # module to draw a board
 class TicTacToe
   def initialize
     @@boardHash = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9 }
     @@current_player = 'X'
     @@victory = false
+    @@draw = false
+    @@positions_filled = 0
   end
 
   def self.drawBoard
@@ -30,26 +33,44 @@ class TicTacToe
     # horizontal
     # vertical
     # diagonal
+    boardHash = @@boardHash
     sets_of_keys = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     match = sets_of_keys.any? { |keys| keys.map { |key| @@boardHash[key] }.uniq.length == 1 }
+    draw = boardHash.all? { |value| %w[X O].include?(value) }
     return unless match
 
     @@victory = true
     puts "Player #{@@current_player} you have won!"
+    puts "#{@@boardHash}"
+  end
+
+  def self.no_victory
+    boardHash = @@boardHash
+    @@positions_filled = @@boardHash.all? { |_key, value| %w[X O].include?(value) if value.is_a?(String)}
+    return unless @@positions_filled && !TicTacToe.check_for_win
+
+    @@draw = true
+    puts "draw"
   end
 
   def start_game
     puts 'Welcome to terminal tic tac toe!'
-
-    while !@@victory
+    loop_count = 0
+    while @@victory == false && @@draw == false
       TicTacToe.drawBoard
       puts "Player #{@@current_player}, select your choice (1-9)"
       choice = gets.chomp.to_i
       if (1..9).include?(choice) && @@boardHash[choice] != 'X' && @@boardHash[choice] != '0'
         TicTacToe.update_board(choice)
         TicTacToe.switch_player
+        loop_count += 1
+       #if loop_count >= 9
+          TicTacToe.check_for_win
+          TicTacToe.no_victory
+          TicTacToe.class_eval { @@boardHash }
+          #binding.pry
+        #end
       end
-      TicTacToe.check_for_win
     end
   end
 end
@@ -59,4 +80,8 @@ end
 game = TicTacToe.new
 game.start_game
 
+# #no code for no victory
+# #can i press same num twice?
+# #open pry debug and see value of position filled incase of draw
 
+# open irb to inspect value

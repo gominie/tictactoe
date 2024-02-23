5a6773f6b22962527e78adfc0134088412ffe87e
gominie <109306648+gominie@users.noreply.com>
2024-02-23 12:17:21 +0000

    board update after win

--- main.rb
+++ main.rb
@@ -1,15 +1,13 @@
-require 'pry-byebug'
-# module to draw a board
 class TicTacToe
   def initialize
     @@boardHash = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9 }
     @@current_player = 'X'
     @@victory = false
     @@draw = false
+    @@choice = 0
   end
 
   def self.drawBoard
-    # boardHash = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9 }
     boardHash = @@boardHash
     counter = 0
 
@@ -23,7 +21,7 @@ class TicTacToe
   def self.switch_player
     if !@@victory && !@@draw
      @@current_player = @@current_player == 'X' ? 'O' : 'X'
-      puts "Switching players. Current player: #{@@current_player}"
+      puts "Current player: #{@@current_player}"
     end
   end
 
@@ -32,54 +30,41 @@ class TicTacToe
   end
 
   def self.check_for_win
-    boardHash = @@boardHash
     sets_of_keys = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
     match = sets_of_keys.any? { |keys| keys.map { |key| @@boardHash[key] }.uniq.length == 1 }
     return unless match
-
     @@victory = true
     puts "Player #{@@current_player} you have won!"
   end
 
   def self.no_victory
-    boardHash = @@boardHash
     positions_filled = @@boardHash.all? { |_key, value| %w[X O].include?(value) }
-    #return unless positions_filled && !TicTacToe.check_for_win
-    if positions_filled && !@@victory
+    return unless positions_filled && !@@victory
      @@draw = true
-    # @@victory = true
-    #TicTacToe.update_board(choice)
      puts 'draw'
-    end
+    #end
   end
 
   def start_game
     puts 'Welcome to terminal tic tac toe!'
+    TicTacToe.drawBoard
     loop_count = 0
     while @@victory == false && @@draw == false
-      TicTacToe.drawBoard
       puts "Player #{@@current_player}, select your choice (1-9)"
-      choice = gets.chomp.to_i
-      next unless (1..9).include?(choice) && @@boardHash[choice] != 'X' && @@boardHash[choice] != '0'
-      TicTacToe.update_board(choice)
+      @@choice = gets.chomp.to_i
+      next unless (1..9).include?(@@choice) && @@boardHash[@@choice] != 'X' && @@boardHash[@@choice] != '0'
+      TicTacToe.update_board(@@choice)
       TicTacToe.check_for_win
       TicTacToe.no_victory
       loop_count += 1
-      # if loop_count >= 9
-
+       TicTacToe.drawBoard
         TicTacToe.switch_player
-
-      #dont remove this line
       break if @@victory || @@draw
-      # binding.pry
-      # end
     end
   end
 end
 
-# rescue/exception code if user doesnt select 1..9
+
 
 game = TicTacToe.new
 game.start_game
-
-# incorrect player declaration
